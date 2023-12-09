c90e47e282c57a1b3f0be1f01c0c0382e38ada42
iyume <iyumelive@gmail.com>
2023-12-09 16:38:12 +0800

    check1: complete 12.20 Gbit/s

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -5,14 +5,98 @@ using namespace std;
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring, Writer& output )
 {
   // Your code here.
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
-  (void)output;
+  if ( output.is_closed() ) {
+    return;
+  }
+  if ( data.empty() ) {
+    if ( is_last_substring ) {
+      output.close();
+    }
+    return;
+  }
+  if ( is_last_substring ) {
+    _last_string_end = first_index + data.size();
+  }
+  if ( first_index + data.size() <= output.bytes_pushed() ) {
+    return;
+  }
+  if ( first_index < output.bytes_pushed() ) {
+    output.push( data.substr( output.bytes_pushed() - first_index ) );
+    _last_popped_end = output.bytes_pushed();
+    if ( is_last_substring ) {
+      if ( first_index + data.size() == output.bytes_pushed() ) {
+        _unassembled_strings.clear();
+        output.close();
+      }
+    } else {
+      _push_availables( output, first_index + data.size() );
+    }
+    return;
+  }
+  if ( !output.available_capacity() ) {
+    return;
+  }
+  if ( first_index == output.bytes_pushed() ) {
+    output.push( data );
+    _last_popped_end = output.bytes_pushed();
+    _push_availables( output, first_index + data.size() );
+    return;
+  }
+  if ( first_index >= output.bytes_pushed() + output.available_capacity() ) {
+    return;
+  }
+  if ( _unassembled_strings.contains( first_index )
+       && _unassembled_strings.at( first_index ).size() >= data.size() ) {
+    return;
+  }
+  if ( first_index + data.size() - output.bytes_pushed() <= output.available_capacity() ) {
+    _unassembled_strings[first_index] = std::move( data );
+  } else {
+    _unassembled_strings[first_index]
+      = data.substr( 0, output.available_capacity() + output.bytes_pushed() - first_index );
+  }
 }
 
 uint64_t Reassembler::bytes_pending() const
 {
   // Your code here.
-  return {};
+  size_t res {};
+  size_t prev_end = _last_popped_end;
+  for ( const auto& it : _unassembled_strings ) { // reversed with break?
+    if ( it.first >= prev_end ) {
+      res += it.second.size();
+      prev_end += it.second.size();
+    } else {
+      prev_end = max( prev_end, it.first + it.second.size() );
+    }
+  }
+  return res;
+}
+
+// Push all available substrings after the given index
+void Reassembler::_push_availables( Writer& output, size_t index )
+{
+  if ( index > output.bytes_pushed() ) {
+    return; // the operation before calling this function not fill in the output
+  }
+  size_t prev_end = _last_popped_end;
+  for ( const auto& it : _unassembled_strings ) {
+    if ( it.first > output.bytes_pushed() ) {
+      break;
+    }
+    if ( it.first >= prev_end ) {
+      prev_end += it.second.size();
+      output.push( it.second );
+    } else {
+      if ( it.first + it.second.size() > prev_end ) {
+        output.push( it.second.substr( prev_end - it.first ) );
+      }
+      prev_end = max( prev_end, it.first + it.second.size() );
+    }
+  }
+  _last_popped_end = output.bytes_pushed();
+  if ( output.bytes_pushed() == _last_string_end ) {
+    _unassembled_strings.clear();
+    output.close();
+  }
 }
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -2,6 +2,7 @@
 
 #include "byte_stream.hh"
 
+#include <map>
 #include <string>
 
 class Reassembler
@@ -29,6 +30,29 @@ public:
    */
   void insert( uint64_t first_index, std::string data, bool is_last_substring, Writer& output );
 
+  //? my note here about the algorithm detail
+  /*
+   * "should discard any bytes that lie beyond the stream's available capacity"?
+   * "The Reassembler’s will not store any bytes that can’t be pushed to the ByteStream either immediately"?
+   * last pushed string should be truncated if not fit capacity.
+   * example("abcdefgh") cap=8 [push "bcd",1] [push "cde",2] [push "g",6] [push "hijk",7]
+   * unassembled strings will be {1:"bcd",2:"cde",6:"g",7:"h"}
+   * 1. "bcd" and "cde" overlapping should be resolved to "bcde".
+   * 2. when received [push "a",0] (even [push "abc",0]), reassember should push all push-able string
+   * (this case is "abcde") and wait for next substring ("f").
+   * 3. "hijk" truncated to "h".
+   * 4. when do [push "cdef", 2], it shoule replace the "cde" to the longer "cdef" on index 2.
+   * as the example unassembled strings above. if bytes_pushed=2 (pushed "ab"), then reassembler
+   * should be able to find "bcd" and push "cd" (such as remember the last bytes_pushed)
+   */
+
   // How many bytes are stored in the Reassembler itself?
   uint64_t bytes_pending() const;
+
+private:
+  //* Reassembler throughput: 12.20 Gbit/s
+  std::map<size_t, std::string> _unassembled_strings;
+  size_t _last_string_end;
+  size_t _last_popped_end;
+  void _push_availables( Writer& output, size_t index );
 };
