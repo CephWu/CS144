500fe51a26b00dcf2079c9b8230084b92420de6d
iyume <iyumelive@gmail.com>
2023-12-09 18:14:19 +0800

    check1: add note

--- src/reassembler.hh
+++ src/reassembler.hh
@@ -35,6 +35,10 @@ public:
    * "should discard any bytes that lie beyond the stream's available capacity"?
    * "The Reassembler’s will not store any bytes that can’t be pushed to the ByteStream either immediately"?
    * last pushed string should be truncated if not fit capacity.
+   *
+   * In abstract, all incoming substrings of the "underlying byte stream" are in random order,
+   * random index and random length. The goal is to reassemble them.
+   *
    * example("abcdefgh") cap=8 [push "bcd",1] [push "cde",2] [push "g",6] [push "hijk",7]
    * unassembled strings will be {1:"bcd",2:"cde",6:"g",7:"h"}
    * 1. "bcd" and "cde" overlapping should be resolved to "bcde".
@@ -44,6 +48,9 @@ public:
    * 4. when do [push "cdef", 2], it shoule replace the "cde" to the longer "cdef" on index 2.
    * as the example unassembled strings above. if bytes_pushed=2 (pushed "ab"), then reassembler
    * should be able to find "bcd" and push "cd" (such as remember the last bytes_pushed)
+   *
+   * 目前的实现有些混乱，因为我一开始误以为传入的子串 index 都是提前分割好固定的，也误以为 overlapping 只是针对
+   * 同 index 而言，花了很多时间去做 test fit。最后才知道是完全随机的。
    */
 
   // How many bytes are stored in the Reassembler itself?
