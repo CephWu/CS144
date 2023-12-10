bb3f2f42c218cdc9ea66c8d01215a119935cfe3a
iyume <iyumelive@gmail.com>
2023-12-10 16:42:49 +0800

    check2: Wrap32 wrap unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,31 @@
 #include "wrapping_integers.hh"
+#include <algorithm>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + n; // n % ( 1LL << 32 )
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  if ( !checkpoint ) { // maybe just for speed up?
+    return raw_value_ - zero_point.raw_value_;
+  }
+  // get absoluate seqno offset uint32
+  uint32_t offset = raw_value_ - zero_point.raw_value_;
+  // calculate absoluate seqno uint64 closest to checkpoint
+  uint32_t count = checkpoint / ( 1LL << 32 );
+  uint32_t ckpt_offset = checkpoint % ( 1LL << 32 );
+  if ( max( offset, ckpt_offset ) - min( offset, ckpt_offset ) > ( 1LL << 31 ) ) {
+    if ( ckpt_offset > offset ) {
+      count += 1;
+    } else if ( count ) {
+      count -= 1;
+    }
+  }
+  return ( 1UL << 32 ) * count + offset;
 }
