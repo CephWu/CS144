a84220f081290c6632e4d3f35c12f6b10d1984f9
iyume <iyumelive@gmail.com>
2023-12-15 12:08:04 +0800

    check3: TCPSender

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,6 +1,8 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
+#include "tcp_sender_message.hh"
 
+#include <algorithm>
 #include <random>
 
 using namespace std;
@@ -13,41 +15,144 @@ TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
   // Your code here.
-  return {};
+  //* Get the length of all segments not acked (no bigger than window size)
+  // TODO(iyume): Optimize
+  uint64_t res {};
+  for ( const auto& it : _messages ) {
+    res += it.second.sequence_length();
+  }
+  return res;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
   // Your code here.
-  return {};
+  return _consecutive_retransmissions;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
   // Your code here.
+  if ( !_send_queue.empty() ) {
+    uint64_t seqno = _send_queue.front();
+    _send_queue.pop();
+    // Retransmittion is possibly not in the flighting messages
+    if ( const auto& msg = _messages.find( seqno ); msg != _messages.end() ) {
+      return msg->second;
+    }
+  }
   return {};
 }
 
 void TCPSender::push( Reader& outbound_stream )
 {
   // Your code here.
-  (void)outbound_stream;
+  //* This function read stream into segments as many as possible
+  //* The sequence number in flight should not beyond the window size
+  //* Specially, pretend window size as one only if no sequence number in flight and no window size
+
+  using value_type = decltype( _messages )::value_type;
+  // New definition is needed because window_size enables RTO back off
+  uint16_t window_size = _window_size;
+  if ( !window_size && !sequence_numbers_in_flight() ) {
+    window_size = 1;
+  }
+
+  if ( _finished ) {
+    return;
+  }
+
+  string payload;
+
+  while ( window_size > sequence_numbers_in_flight() ) {
+    bool syn {};
+    bool fin {};
+    uint64_t abs_seqno = 1 + outbound_stream.bytes_popped();
+    uint16_t read_size = window_size - sequence_numbers_in_flight();
+
+    if ( !_started ) {
+      _started = true;
+      syn = true;
+      read_size -= 1;
+      abs_seqno -= 1;
+    }
+    // TEST: MAX_PAYLOAD_SIZE limits payload only
+    uint16_t payload_max_size = min( read_size, static_cast<uint16_t>( TCPConfig::MAX_PAYLOAD_SIZE ) );
+    read( outbound_stream, payload_max_size, payload );
+    read_size -= payload.size();
+    if ( !_finished && outbound_stream.is_finished() && read_size ) {
+      _finished = true;
+      fin = true;
+    }
+    if ( !( syn + payload.size() + fin ) ) {
+      break; // nothing to send
+    }
+
+    _messages.emplace( value_type { abs_seqno, { isn_ + abs_seqno, syn, { payload }, fin } } );
+    _send_queue.emplace( abs_seqno );
+  }
+
+  _popped_bytes = outbound_stream.bytes_popped();
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
   // Your code here.
-  return {};
+  return { .seqno = isn_ + _started + _popped_bytes + _finished };
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
   // Your code here.
-  (void)msg;
+  //* In the tests, the ACK triggers push method to call
+  //* TEST "SYN + FIN", is it possible to receive msg without ackno and take new window size?
+
+  _window_size = msg.window_size; // update unconditional
+
+  if ( msg.ackno ) {
+    // just find the closest ackno to last ackno, but not to the stream's end (maybe more accurate)
+    uint64_t ackno = msg.ackno->unwrap( isn_, _last_ackno );
+    if ( ( _last_ackno && ackno <= _last_ackno ) || ackno > _started + _popped_bytes + _finished ) {
+      // (_last_ackno && ackno <= _last_ackno) is a trick to let timer not restart
+      // because _last_ackno is not initialized less than 0
+      return;
+    }
+    _last_ackno = ackno;
+
+    //? try some lower_bound trick?
+    // _messages.erase( _messages.begin(), --_messages.lower_bound( ackno ) );
+    auto erase_end = _messages.begin();
+    while ( erase_end != _messages.end() ) {
+      if ( ackno < erase_end->first + erase_end->second.sequence_length() ) {
+        break;
+      }
+      erase_end++;
+    }
+    _messages.erase( _messages.begin(), erase_end );
+
+    _rto_factor = 1;
+    _consecutive_retransmissions = 0;
+    _ms_since_last_ticked = 0;
+  }
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
   // Your code here.
-  (void)ms_since_last_tick;
+  if ( _messages.empty() ) {
+    return;
+  }
+  _ms_since_last_ticked += ms_since_last_tick;
+  if ( _ms_since_last_ticked >= initial_RTO_ms_ * _rto_factor ) {
+    _send_queue.emplace( _messages.begin()->first );
+    if ( _window_size ) {
+      //* Back off RTO only if the network is lousy.
+      //* The zero window size indicates that receiver is busy, and there is only one special
+      //* message in the flighting messages which only contains one length.
+      _rto_factor *= 2;
+      _consecutive_retransmissions += 1;
+    }
+    _ms_since_last_ticked = 0;
+    return;
+  }
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,10 +4,36 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
+#include <map>
+#include <optional>
+#include <queue>
+
+//* TCPSender 的 handout 有很多细节没讲清楚，这里做点笔记。
+//* 根本没讲清为什么要在 window size != 0 时增加累计重发次数并 back off RTO。
+//* 原文说在 push 的时候对 window size 0 特殊处理，也没讲清楚，和上一条是关联的。
+//* 还有 window size 的概念，也没讲为什么每次 receive 就需要执行无条件更新，因为 ackno + window size -
+//* pushed byte count 可以得到精确值，我第一次做的时候是这么写的，但是卡在了 send_extra 一些莫名其妙的测试上，
+//* 后来是卡在了 "Repeated ACKs and outdated ACKs are harmless"，
+//* 继续写的话应该可以过测试，但是代码太丑了就重写了。
+//* 还有 tick 那里，旨在是为了计时第一个发出的包然后重发，但是理论上，只要在
+//* tick 时判断一下有没有 outstanding segment 再增加计时，收到 ack new receipt of data 再重置，
+//* 就可以保证计时是精确的，事实上这么实现的可以通过 tick 所有测试。
+//* 总而言之，handout 没讲明白原理和细节，只讲流程，根本没有发挥空间。
+
 class TCPSender
 {
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+  uint16_t _rto_factor { 1 };
+  uint64_t _ms_since_last_ticked {}; // cumulative time since last called
+  uint64_t _consecutive_retransmissions {};
+  std::queue<uint64_t> _send_queue;
+  std::map<uint64_t, TCPSenderMessage> _messages; // abs seqno to message
+  uint64_t _popped_bytes {};                      // use bytes_popped() to get abs seqno
+  bool _started {};                               // mark SYN written
+  bool _finished {};                              // mark FIN written
+  uint64_t _last_ackno {};
+  uint64_t _window_size { 1 }; // as said in FAQ (for back off RTO)
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
